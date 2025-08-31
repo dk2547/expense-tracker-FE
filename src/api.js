022b171defe4e3179a7b5794b44f4ce882e3d979
akriti <akritichandra01@gmail.com>
2025-08-31 19:14:55 +0530

    report

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const getApiEndPoints = (hostURL='')=>{
-    return {
-        GET_CATAGORY: `${hostURL}/expense/category`,
-        EXPENSE: `${hostURL}/expense`,
-        DELETE_EXPENSE: `${hostURL}/expense/`,
-        GET_REPORT: `${hostURL}/reports`
-    }
-}
+export const getApiEndPoints = (hostURL = "") => {
+  return {
+    GET_CATAGORY: `${hostURL}/expense/category`,
+    EXPENSE: `${hostURL}/expense`,
+    DELETE_EXPENSE: `${hostURL}/expense/`,
+    GET_REPORT: `${hostURL}/api/reports`,
+  };
+};
--- src/components/Report.js
+++ src/components/Report.js
@@ -18,146 +18,7 @@ export default function Report() {
   const [monthYearList, setMonthYearList] = useState([]);
   const [monthlyBarData, setMonthlyBarData] = useState([]);
   const [monthlyContribution, setMonthlyContribution] = useState([]);
-  const [reportData, setReport] = useState({
-    categoryWiseReport: [
-      {
-        monthYear: "2025/2",
-        totalExpense: 92258.0,
-        categoryWiseExpense: {
-          "Doctor/ medicines": 137.0,
-          Gift: 1220.0,
-          miscellaneous: 11581.0,
-          Vacation: 20838.0,
-          Groceries: 4768.0,
-          "Personal Care": 250.0,
-          "Electricity/Recharge": 4182.0,
-          "House Rent": 28096.0,
-          "Home & Kitchen": 13477.0,
-          Transport: 746.0,
-          "Outside Food": 5075.0,
-          "Entertainment/ hobbies": 1888.0,
-        },
-        contributorWiseExpense: {
-          Akriti: 56051.0,
-          Sameer: 36207.0,
-        },
-      },
-      {
-        monthYear: "2025/3",
-        totalExpense: 141549.11,
-        categoryWiseExpense: {
-          "Doctor/ medicines": 720.0,
-          miscellaneous: 1959.0,
-          Groceries: 19506.11,
-          "Personal Care": 889.0,
-          "Electricity/Recharge": 653.0,
-          Clothing: 1212.0,
-          "Personal growth": 9000.0,
-          "Entertainment/ hobbies": 10937.0,
-          Gift: 32696.0,
-          "House Rent": 9480.0,
-          "Home & Kitchen": 4542.0,
-          Transport: 34425.0,
-          maid: 10028.0,
-          "Outside Food": 5502.0,
-        },
-        contributorWiseExpense: {
-          Akriti: 71127.11,
-          Sameer: 70422.0,
-        },
-      },
-      {
-        monthYear: "2025/4",
-        totalExpense: 155641.0,
-        categoryWiseExpense: {
-          "Doctor/ medicines": 494.0,
-          Vacation: 83954.0,
-          miscellaneous: 10787.0,
-          Groceries: 4847.0,
-          "Personal Care": 1400.0,
-          "Electricity/Recharge": 743.0,
-          "Personal growth": 7500.0,
-          Clothing: 7205.0,
-          "Entertainment/ hobbies": 1676.0,
-          Gift: 647.0,
-          "House Rent": 25440.0,
-          "Home & Kitchen": 4881.0,
-          Transport: 872.0,
-          "Outside Food": 5195.0,
-        },
-        contributorWiseExpense: {
-          Akriti: 81865.0,
-          Sameer: 73776.0,
-        },
-      },
-      {
-        monthYear: "2025/5",
-        totalExpense: 102530.0,
-        categoryWiseExpense: {
-          "Doctor/ medicines": 4613.0,
-          Gift: 8562.0,
-          Groceries: 4156.0,
-          miscellaneous: 5997.0,
-          "Personal Care": 400.0,
-          "Electricity/Recharge": 1439.0,
-          "House Rent": 25415.0,
-          "Personal growth": 1377.0,
-          "Home & Kitchen": 23219.0,
-          Transport: 9270.0,
-          "Outside Food": 7003.0,
-          "Entertainment/ hobbies": 11079.0,
-        },
-        contributorWiseExpense: {
-          Akriti: 66278.0,
-          Sameer: 36252.0,
-        },
-      },
-      {
-        monthYear: "2025/6",
-        totalExpense: 179287.0,
-        categoryWiseExpense: {
-          Gift: 88064.0,
-          Vacation: 40885.0,
-          miscellaneous: 3093.0,
-          Groceries: 5174.0,
-          "Personal Care": 3580.0,
-          "House Rent": 25900.0,
-          "Home & Kitchen": 1263.0,
-          Clothing: 1948.0,
-          donation: 100.0,
-          Transport: 2022.0,
-          "Outside Food": 6725.0,
-          "Entertainment/ hobbies": 533.0,
-        },
-        contributorWiseExpense: {
-          Akriti: 57260.0,
-          Sameer: 122027.0,
-        },
-      },
-      {
-        monthYear: "2025/7",
-        totalExpense: 112730.0,
-        categoryWiseExpense: {
-          "Doctor/ medicines": 280.0,
-          Gift: 735.0,
-          Vacation: 28630.0,
-          miscellaneous: 3947.0,
-          Groceries: 7588.0,
-          "Personal Care": 3072.0,
-          "House Rent": 25200.0,
-          "Home & Kitchen": 26500.0,
-          maid: 7323.0,
-          Transport: 1740.0,
-          "Outside Food": 3813.0,
-          "Entertainment/ hobbies": 3902.0,
-        },
-        contributorWiseExpense: {
-          Akriti: 54742.0,
-          Sameer: 57988.0,
-        },
-      },
-    ],
-  });
+  const [reportData, setReport] = useState(null);
   const [barChartData, setBarChartData] = useState([]);
 
   useEffect(() => {
@@ -165,11 +26,12 @@ export default function Report() {
   }, []);
 
   const getTableData = async () => {
-    // try {
-    //   const fetchedData = await apiService.getReport();
-    // } catch (err) {
-    // } finally {
-    // }
+    try {
+      const fetchedData = await apiService.getReport();
+      setReport(fetchedData);
+    } catch (err) {
+    } finally {
+    }
   };
 
   useEffect(() => {
@@ -180,7 +42,6 @@ export default function Report() {
 
   const createBarChartData = () => {
     if (!reportData || !reportData.categoryWiseReport) {
-      console.error("no Data found");
       return;
     }
     const formattedData = reportData?.categoryWiseReport?.map((item) => ({
@@ -190,11 +51,9 @@ export default function Report() {
     const yearMonthList = reportData?.categoryWiseReport?.map(
       (item) => item.monthYear
     );
-    console.log(yearMonthList);
     setMonthYearList(yearMonthList);
-    console.log(formattedData);
     setBarChartData(formattedData);
-    setYearMonth(yearMonthList[5]);
+    setYearMonth(yearMonthList[yearMonthList.length - 1]);
   };
 
   const handleChange = (e) => {
@@ -206,7 +65,6 @@ export default function Report() {
   }, [yearMonth]);
 
   const createMonthlyReport = (yearMonth) => {
-    console.log(yearMonth);
     if (!yearMonth) {
       return;
     }
@@ -221,11 +79,16 @@ export default function Report() {
       });
       setMonthlyBarData(monthlyBarData);
     }
+    const monthlyContributerData = reportData.contributorWiseReport.find(
+      (item) => {
+        return item.monthYear === yearMonth;
+      }
+    );
     const monthlyPieData = [];
-    for (let key in monthlyData.contributorWiseExpense) {
+    for (let key in monthlyContributerData.contributorWiseExpense) {
       monthlyPieData.push({
         name: key,
-        value: monthlyData.contributorWiseExpense[key],
+        value: monthlyContributerData.contributorWiseExpense[key],
       });
       setMonthlyContribution(monthlyPieData);
     }
@@ -267,7 +130,7 @@ export default function Report() {
         </label>
       </div>
       <div className="row">
-        <div className="col-6">
+        <div className="col-12">
           <ResponsiveContainer width="100%" height={400}>
             <BarChart width={730} height={250} data={monthlyBarData}>
               <CartesianGrid strokeDasharray="3 3" />
