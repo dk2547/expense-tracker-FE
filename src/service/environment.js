772c0623f2571be1156c8b0a994642d92002ebf0
dk2547 <37217665+dk2547@users.noreply.github.com>
2026-03-07 18:44:57 +0530

    Update environment URLs to use local domain

--- src/service/environment.js
+++ src/service/environment.js
@@ -1,7 +1,7 @@
 import {getApiEndPoints} from '../api';
 const environmentList = {
-    STG: 'http://raspberrypi:9091',
-    PROD: 'http://raspberrypi:9090',
+    STG: 'http://raspberrypi.local:9091',
+    PROD: 'http://raspberrypi.local:9090',
 };
 const env = window._env_.REACT_APP_ENV;
 // const env = process.env.REACT_APP_ENV;
